e0fb1f79a475cfa5fe8b9d39012ab3da5481bee5
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-09-03 19:57:17 +0530

    add some component to project

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,9 @@
+
+
+const SocialMedia = () => {
+  return (
+    <div>Social_media_login</div>
+  )
+}
+
+export default SocialMedia;
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import LoginForm from "../components/LoginForm"
+import SocialMedia from "../components/SocialMedia"
+
+const Login = () => {
+  return (
+    <div>
+        <SocialMedia/>
+        <LoginForm/>
+    </div>
+  )
+}
+
+export default Login
