408e41e218721ebd159ceb5bdbeb6aad3838bf81
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-09-04 21:49:07 +0530

    make changes to gitignore file

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Settings = () => {
   return (
-    <div className='p-8'>
-     <div className="w-[280px] navbar_background h-[90vh] rounded-2xl"> 
+    <div className="p-8">
+      <div className="w-[280px] navbar_background h-[90vh] rounded-2xl">
         <Navbar />
       </div>
-    <div className='text-4xl text-center font-bold mt-[15%]'> Settings</div>
+      <div className="text-4xl text-center font-bold mt-[15%]">Settings</div>
     </div>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 const Users = () => {
   return (
-    <div className='text-4xl text-center font-bold mt-[15%]'>Users</div>
-  )
-}
+    <div className="p-8">
+      <div className="w-[280px] navbar_background h-[90vh] rounded-2xl">
+        <Navbar />
+      </div>
+      <div className="text-4xl text-center font-bold mt-[15%]">Users</div>
+    </div>
+  );
+};
 
-export default Users
+export default Users;
